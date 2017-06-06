d7f53123a343fd2fcb9cc1ed62bf069ec3c4425e
carlos <carsandeu@gmail.com>
2017-06-06 15:11:12 +0200

    Incluyo la carpeta de Scripts como segundo COMMIT

---
+++ Scripts/html_template.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+####MAIN=Generate HTML Template
+cat << _EOF_ 
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	<title></title>
+	</head>
+
+	<body>
+
+	</body>
+
+</html>
+_EOF_
